67f19d58e54ae5d74cc0ce8648f487918f78e8e0
Vansh909011 <vr3725793@gmail.com>
2025-10-24 09:46:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="7searchppc" content="6197ab57a34d324d093b9f190e28f6c3"/>
+  <title>7SearchPPC Verification Page</title>
+</head>
+<body>
+  <h2>7SearchPPC Verification Successful Page</h2>
+  <p>If you can see this page, your verification file is uploaded correctly.</p>
+</body>
+</html>
